e4d2e025b6edc5dc410892c33635a8fbcad1830b
Aman Patel <Amanpatel5229@gmail.com>
2023-03-02 11:49:49 +0530

    second commit

---
+++ wealth1.java
@@ -0,0 +1,20 @@
+class wealth1
+{
+	public static void main(String args[])
+	{
+		int arr[][]={{1,4,2},{5,7,9},{4,2,8,9},{5,1,8}};
+		int wealth=0;
+		
+		for(int column=0;column<arr.length;column++)
+		{
+			int sum=0;
+			for(int row=0;row<arr[column].length;row++)
+			{
+				sum+=arr[column][row];
+			}
+			if(sum>wealth)
+				wealth=sum;
+		}
+		System.out.println(wealth);
+	}
+}
