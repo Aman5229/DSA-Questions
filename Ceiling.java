0b2243b6aa7aa2ba1907b8b6e435e73532c166e6
Aman Patel <121476667+Aman5229@users.noreply.github.com>
2023-03-04 10:28:24 +0530

    Ceiling of a number

---
+++ Ceiling.java
@@ -0,0 +1,33 @@
+// FIND THE CEILING OF THE SORTED ARRAY
+class Ceiling
+{
+	public static void main(String args[])
+	{
+		int []arr={2,3,5,9,14,16,18,45,88,90,96,99};
+		int target=17;
+		System.out.println(Search(arr, target));
+	}
+	static int Search(int arr[], int target)
+	{
+		int start=0;
+		int end=arr.length-1;
+		while(start<=end)
+		{
+			int mid=start+(end-start)/2;
+			if(arr[mid]==target)
+			{
+				return arr[mid];
+			}
+			else if(arr[mid]>target)
+			{
+				end=mid-1;
+			}
+			else if(arr[mid]<target)
+			{
+				start=mid+1;
+			}
+		}
+		
+		return arr[end];
+	}
+}
